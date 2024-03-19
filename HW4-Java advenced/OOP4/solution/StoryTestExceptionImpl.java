0ab96f70cd14660d6a0f92cb9035ef3de2a18943
MarinaYanovskiy <marinay@campus.technion.ac.il>
2024-03-19 12:40:13 +0200

    implement storyTestException

--- HW4-Java advenced/OOP4/solution/StoryTestExceptionImpl.java
+++ HW4-Java advenced/OOP4/solution/StoryTestExceptionImpl.java
@@ -1,2 +1,37 @@
-package solution;public class StoryTestExceptionImpl {
+package solution;
+
+import provided.StoryTestException;
+
+public class StoryTestExceptionImpl extends StoryTestException {
+    private int numFails;
+    private String firstFailedSentence;
+    private String expected;
+    private String result;
+
+    public StoryTestExceptionImpl(int numFails, String firstFailedSentence, String expected, String result) {
+        this.numFails = numFails;
+        this.firstFailedSentence = firstFailedSentence;
+        this.expected = expected;
+        this.result = result;
+    }
+
+    @Override
+    public String getSentance() {
+        return firstFailedSentence;
+    }
+
+    @Override
+    public String getStoryExpected() {
+        return expected;
+    }
+
+    @Override
+    public String getTestResult() {
+        return result;
+    }
+
+    @Override
+    public int getNumFail() {
+        return numFails;
+    }
 }
